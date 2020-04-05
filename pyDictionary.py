7d678a0dd37a86f03be7c20110b802c16e38e51f
Oniel Toledo <toledooniel@gmail.com>
2020-04-05 13:49:15 -0700

    Added functionality to return suggested words in the case that the user enters a word that could not be found in the dictionary. Simplified program exit steps

--- pyDictionary.py
+++ pyDictionary.py
@@ -1,20 +1,29 @@
+import difflib
 import json
 
-escape = '-quit'
-
 with open("dictionary.json") as dictionary_file:
-
     dictionary_obj = json.load(dictionary_file)
+
+    print('Welcome to pyDictionary (click "Enter" twice in blank rows to exit program)')
     
-    print('Welcome to pyDictionary ( to end program enter "%s")' % escape)
-    
+    blank_count = 0
     while True:
+        if(blank_count >= 2): break
+
         user_question = 'Enter word to look up'
         word_to_find = input('%s: ' % user_question)
-        if word_to_find == escape:
-            break
+
+        if word_to_find == '':
+            blank_count += 1
         else:
-            definitions = dictionary_obj.get(word_to_find, ['No definition could be found for the keyword "%s"' % word_to_find])
-            for index, definition in enumerate(definitions):
-                print('    %s) %s' % (index, definition))
+            blank_count = 0
+
+            definitions = dictionary_obj.get(word_to_find)
+            if definitions == None:
+                # find similar words
+                print('\tCould not a definition for the provided word. Did you mean one of the following:')
+                print('\t%s' % ', '.join(difflib.get_close_matches(word_to_find, dictionary_obj.keys(), n = 6)) )
+            else:
+                for index, definition in enumerate(definitions):
+                    print('\t%s) %s' % (index, definition))
             print()
